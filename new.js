28649f0de3d97d7898aac457636ac66aa55db7ca
rasel <russelroy15@gmail.com>
2021-08-28 00:11:40 +0600

    new code added

---
+++ new.js
@@ -0,0 +1,28 @@
+const data = {
+    result: [{
+        userName: {
+            title: 'Mr.',
+            name: "sakib khan"
+        }
+    }]
+}
+// const name = data.result.userName.name
+const name = data.result[0].userName.name
+// const name = data.result[1].userName.name
+// const name = data.result.userName[0].name
+
+// console.log(name)
+
+
+// const path = "images/cat.jpg";
+
+// const div = document.createElement('div');
+
+// div.innerHTML = `<img src=__?__ alt = "">`;
+
+const bondCode = ` I am Fake James bond . My new code is: 00${7 + 1 + 2}`
+// console.log(bondCode);
+
+// fetch(url).then(res => res.json()).then(data => console.log(data).then(error => console.log(error));
+// fetch(url).then(res => res.json()).then(data => console.log(data).error(error => console.log(error));
+// fetch(url).then(res => res.json()).then(data => console.log(data).catch(error => console.log(error));
